e5d6c08ea64526d4f1ce23ea42821c3bd8da70cb
Olaniyi Olabode <Olaniyiolabode99@yahoo.com>
2022-10-29 16:21:54 +0100

    final commit

--- controllers/trackOneController.js
+++ controllers/trackOneController.js
@@ -1,13 +1,18 @@
-let data = {
-  slackUsername: "Niero",
-  backend: true,
-  age: 28,
-  bio: "Software / Engineer",
-};
+// let data = {
+//   slackUsername: "Niero",
+//   backend: true,
+//   age: 28,
+//   bio: "Software / Engineer",
+// };
 
 module.exports.getData = async (req, res) => {
   try {
-    res.status(200).json({ data });
+    res.status(200).json({
+      slackUsername: "Niero",
+      backend: true,
+      age: 28,
+      bio: "Software / Engineer",
+    });
   } catch (err) {
     res.status(404).json({ errors: { msg: "Something went wrong." } });
   }
